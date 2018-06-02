fdec6d45eb3a9f6359a91764df707b8521c12631
Raúl <raul2040@outlook.es>
2018-06-02 23:56:00 +0200

    modificando un poco el estilo e intentando que funcione en heroku

--- db/migrate/20180524063157_add_user_id_to_articles.rb
+++ db/migrate/20180524063157_add_user_id_to_articles.rb
@@ -1,6 +1,6 @@
 class AddUserIdToArticles < ActiveRecord::Migration
   def change
     add_reference :articles, :user, index: true, foreign_key: true
-    
+
   end
 end
--- db/migrate/20180524073920_create_comments.rb
+++ db/migrate/20180524073920_create_comments.rb
@@ -1,7 +1,6 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
-      t.references :user, index: true, foreign_key: true
       t.references :article, index: true, foreign_key: true
       t.text :body
 
